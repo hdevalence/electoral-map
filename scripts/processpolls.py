535fef8f31b91e2b9015212dfad01ce97af302c5
Henry de Valence <hdevalence@hdevalence.ca>
2014-01-05 23:14:59 -0500

    Simplify query logic by precomputing polls to merge

--- scripts/processpolls.py
+++ scripts/processpolls.py
@@ -40,12 +40,35 @@ def add_votes(cur, fednum, emrp_name, party, votes):
                 , 'emrp_name' : emrp_name
                 })
 
+def get_psnums(cur, fednum, emrp_name):
+    """
+    Get all polling station numbers from pollresults which
+    should be combined into the row for emrp_name.
+    """
+    cur.execute("""SELECT array_agg(DISTINCT psnum) FROM pollresults WHERE
+                           ednum = %(fednum)s
+                   AND NOT voidpoll
+                   AND NOT nopoll
+                   AND (   (   mergewith = ''
+                           AND (psnum = %(psnum)s OR psnum LIKE %(psnum_letter)s)
+                           )
+                       OR  mergewith = %(psnum)s OR mergewith LIKE %(psnum_letter)s
+                       );""",
+                { 'psnum' : emrp_name
+                , 'psnum_letter' : emrp_name + '_' # Matches, e.g., '26A'
+                , 'fednum' : fednum
+                })
+    return cur.fetchone()[0]
+
 def process_poll(cur, fednum, emrp_name):
     """
     Adds a new entry to the cleanpoll table for the specified poll.
     """
+    # Create row
     cur.execute("INSERT INTO cleanpoll (fednum, emrp_name) VALUES (%s,%s)",
                 (fednum, emrp_name))
+
+    psnums = get_psnums(cur, fednum, emrp_name)
     # Map of party columns to search patterns
     parties = { 'libvotes' : '%Liberal%'
               , 'ndpvotes' : '%NDP%'
@@ -58,39 +81,21 @@ def process_poll(cur, fednum, emrp_name):
     for party, partypattern in parties.items():
         cur.execute("""SELECT sum(votes) FROM pollresults WHERE
                            ednum = %(fednum)s
-                       AND NOT voidpoll
-                       AND NOT nopoll
-                       AND (   (   mergewith = ''
-                               AND (psnum = %(psnum)s OR psnum LIKE %(psnum_letter)s)
-                               )
-                           OR  mergewith = %(psnum)s OR mergewith LIKE %(psnum_letter)s
-                           )
+                       AND psnum = ANY(%(psnums)s)
                        AND party_en LIKE %(partypattern)s;""",
                     { 'fednum' : fednum
-                    , 'psnum' : emrp_name
-                    , 'psnum_letter' : emrp_name + '_'
+                    , 'psnums' : psnums
                     , 'partypattern' : partypattern
                     })
         partyvotes, = cur.fetchone()
         add_votes(cur, fednum, emrp_name, party, partyvotes)
-    # This duplication is quite ugly.
-    # Also, it would be best to build the query string from the parties dict.
-    # But psycopg does its own query building and it's a mess.
     # Find the number of votes for independent candidates.
-    params = { 'fednum' : fednum
-             , 'psnum' : emrp_name
-             , 'psnum_letter' : emrp_name + '_'
-             }
-    params.update(parties)
+    params = parties
+    params['fednum'] = fednum
+    params['psnums'] = psnums
     cur.execute("""SELECT sum(votes) FROM pollresults WHERE
                        ednum = %(fednum)s
-                   AND NOT voidpoll
-                   AND NOT nopoll
-                   AND (   (   mergewith = ''
-                           AND (psnum = %(psnum)s OR psnum LIKE %(psnum_letter)s)
-                           )
-                       OR  mergewith = %(psnum)s OR mergewith LIKE %(psnum_letter)s
-                       )
+                   AND psnum = ANY(%(psnums)s)
                    AND NOT (   party_en LIKE %(libvotes)s
                            OR  party_en LIKE %(convotes)s
                            OR  party_en LIKE %(ndpvotes)s
