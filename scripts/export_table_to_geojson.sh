e5dfd0ae33bd2893035c9fe514a00733ca32fff7
Henry de Valence <hdevalence@hdevalence.ca>
2014-01-09 01:47:12 -0500

    Fix geojson output script to use correct input CRS

--- scripts/export_table_to_geojson.sh
+++ scripts/export_table_to_geojson.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-# exports table given as $1 to file $2.
+# exports data for riding with fed_num $1 to file $2
 
-ogr2ogr -f "GeoJSON" $2 -s_srs EPSG:4269 -t_srs EPSG:4326 PG:"host=localhost dbname=electoralmap" $1
+ogr2ogr -where "fed_num = $1" -f "GeoJSON" $2 -s_srs EPSG:3347 -t_srs EPSG:4326 PG:"host=localhost dbname=electoralmap" merged_pd_a
