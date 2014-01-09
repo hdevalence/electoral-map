880cd78eca9e67002e4f3682e2cc84a7d3ecdfeb
Henry de Valence <hdevalence@hdevalence.ca>
2014-01-09 01:44:02 -0500

    Add skeleton of site.

---
+++ js/index.js
@@ -0,0 +1,341 @@
+var fed_nums = { "Avalon" : 10001
+               , "Bonavista–Gander–Grand Falls–Windsor" : 10002
+               , "Humber–St. Barbe–Baie Verte" : 10003
+               , "Labrador" : 10004
+               , "Random–Burin–St. George's" : 10005
+               , "St. John's East" : 10006
+               , "St. John's South–Mount Pearl" : 10007
+               , "Cardigan" : 11001
+               , "Charlottetown" : 11002
+               , "Egmont" : 11003
+               , "Malpeque" : 11004
+               , "Cape Breton–Canso" : 12001
+               , "Central Nova" : 12002
+               , "Dartmouth–Cole Harbour" : 12003
+               , "Halifax" : 12004
+               , "Halifax West" : 12005
+               , "Kings–Hants" : 12006
+               , "Cumberland–Colchester–Musquodoboit Valley" : 12007
+               , "Sackville–Eastern Shore" : 12008
+               , "South Shore–St. Margaret's" : 12009
+               , "Sydney–Victoria" : 12010
+               , "West Nova" : 12011
+               , "Acadie–Bathurst" : 13001
+               , "Beauséjour" : 13002
+               , "Fredericton" : 13003
+               , "Fundy Royal" : 13004
+               , "Madawaska–Restigouche" : 13005
+               , "Miramichi" : 13006
+               , "Moncton–Riverview–Dieppe" : 13007
+               , "New Brunswick Southwest" : 13008
+               , "Saint John" : 13009
+               , "Tobique–Mactaquac" : 13010
+               , "Abitibi–Témiscamingue" : 24001
+               , "Ahuntsic" : 24002
+               , "Alfred-Pellan" : 24003
+               , "Argenteuil–Papineau–Mirabel" : 24004
+               , "Beauce" : 24005
+               , "Beauharnois–Salaberry" : 24006
+               , "Beauport–Limoilou" : 24007
+               , "Berthier–Maskinongé" : 24008
+               , "Bourassa" : 24009
+               , "Brome–Missisquoi" : 24010
+               , "Brossard–La Prairie" : 24011
+               , "Chambly–Borduas" : 24012
+               , "Charlesbourg–Haute-Saint-Charles" : 24013
+               , "Montmorency–Charlevoix–Haute-Côte-Nord" : 24014
+               , "Châteauguay–Saint-Constant" : 24015
+               , "Chicoutimi–Le Fjord" : 24016
+               , "Compton–Stanstead" : 24017
+               , "Drummond" : 24018
+               , "Gaspésie–Îles-de-la-Madeleine" : 24019
+               , "Gatineau" : 24020
+               , "Hochelaga" : 24021
+               , "Honoré-Mercier" : 24022
+               , "Hull–Aylmer" : 24023
+               , "Jeanne-Le Ber" : 24024
+               , "Joliette" : 24025
+               , "Jonquière–Alma" : 24026
+               , "Lac-Saint-Louis" : 24027
+               , "La Pointe-de-l'Île" : 24028
+               , "LaSalle–Émard" : 24029
+               , "Laurentides–Labelle" : 24030
+               , "Laurier–Sainte-Marie" : 24031
+               , "Laval" : 24032
+               , "Laval–Les Îles" : 24033
+               , "Lévis–Bellechasse" : 24034
+               , "Longueuil–Pierre-Boucher" : 24035
+               , "Lotbinière–Chutes-de-la-Chaudière" : 24036
+               , "Louis-Hébert" : 24037
+               , "Louis-Saint-Laurent" : 24038
+               , "Manicouagan" : 24039
+               , "Marc-Aurèle-Fortin" : 24040
+               , "Haute-Gaspésie–La Mitis–Matane–Matapédia" : 24041
+               , "Mégantic–L'Érable" : 24042
+               , "Montcalm" : 24043
+               , "Mount Royal" : 24044
+               , "Notre-Dame-de-Grâce–Lachine" : 24045
+               , "Abitibi–Baie-James–Nunavik–Eeyou" : 24046
+               , "Outremont" : 24047
+               , "Papineau" : 24048
+               , "Pierrefonds–Dollard" : 24049
+               , "Pontiac" : 24050
+               , "Portneuf–Jacques-Cartier" : 24051
+               , "Québec" : 24052
+               , "Repentigny" : 24053
+               , "Bas-Richelieu–Nicolet–Bécancour" : 24054
+               , "Richmond–Arthabaska" : 24055
+               , "Rimouski-Neigette–Témiscouata–Les Basques" : 24056
+               , "Rivière-des-Mille-Îles" : 24057
+               , "Montmagny–L'Islet–Kamouraska–Rivière-du-Loup" : 24058
+               , "Rivière-du-Nord" : 24059
+               , "Roberval–Lac-Saint-Jean" : 24060
+               , "Rosemont–La Petite-Patrie" : 24061
+               , "Saint-Bruno–Saint-Hubert" : 24062
+               , "Saint-Hyacinthe–Bagot" : 24063
+               , "Saint-Jean" : 24064
+               , "Saint-Lambert" : 24065
+               , "Saint-Laurent–Cartierville" : 24066
+               , "Saint-Léonard–Saint-Michel" : 24067
+               , "Saint-Maurice–Champlain" : 24068
+               , "Shefford" : 24069
+               , "Sherbrooke" : 24070
+               , "Terrebonne–Blainville" : 24071
+               , "Trois-Rivières" : 24072
+               , "Vaudreuil-Soulanges" : 24073
+               , "Verchères–Les Patriotes" : 24074
+               , "Westmount–Ville-Marie" : 24075
+               , "Ajax–Pickering" : 35001
+               , "Algoma–Manitoulin–Kapuskasing" : 35002
+               , "Ancaster–Dundas–Flamborough–Westdale" : 35003
+               , "Barrie" : 35004
+               , "Beaches–East York" : 35005
+               , "Bramalea–Gore–Malton" : 35006
+               , "Brampton–Springdale" : 35007
+               , "Brampton West" : 35008
+               , "Brant" : 35009
+               , "Burlington" : 35010
+               , "Cambridge" : 35011
+               , "Carleton–Mississippi Mills" : 35012
+               , "Chatham-Kent–Essex" : 35013
+               , "Durham" : 35014
+               , "Davenport" : 35015
+               , "Don Valley East" : 35016
+               , "Don Valley West" : 35017
+               , "Dufferin–Caledon" : 35018
+               , "Eglinton–Lawrence" : 35019
+               , "Elgin–Middlesex–London" : 35020
+               , "Essex" : 35021
+               , "Etobicoke Centre" : 35022
+               , "Etobicoke–Lakeshore" : 35023
+               , "Etobicoke North" : 35024
+               , "Glengarry–Prescott–Russell" : 35025
+               , "Bruce–Grey–Owen Sound" : 35026
+               , "Guelph" : 35027
+               , "Haldimand–Norfolk" : 35028
+               , "Haliburton–Kawartha Lakes–Brock" : 35029
+               , "Halton" : 35030
+               , "Hamilton Centre" : 35031
+               , "Hamilton East–Stoney Creek" : 35032
+               , "Hamilton Mountain" : 35033
+               , "Huron–Bruce" : 35034
+               , "Kenora" : 35035
+               , "Kingston and the Islands" : 35036
+               , "Kitchener Centre" : 35037
+               , "Kitchener–Conestoga" : 35038
+               , "Kitchener–Waterloo" : 35039
+               , "Lanark–Frontenac–Lennox and Addington" : 35040
+               , "Leeds–Grenville" : 35041
+               , "London–Fanshawe" : 35042
+               , "London North Centre" : 35043
+               , "London West" : 35044
+               , "Markham–Unionville" : 35045
+               , "Lambton–Kent–Middlesex" : 35046
+               , "Mississauga–Brampton South" : 35047
+               , "Mississauga East–Cooksville" : 35048
+               , "Mississauga–Erindale" : 35049
+               , "Mississauga South" : 35050
+               , "Mississauga–Streetsville" : 35051
+               , "Nepean–Carleton" : 35052
+               , "Newmarket–Aurora" : 35053
+               , "Niagara Falls" : 35054
+               , "Niagara West–Glanbrook" : 35055
+               , "Nickel Belt" : 35056
+               , "Nipissing–Timiskaming" : 35057
+               , "Northumberland–Quinte West" : 35058
+               , "Oak Ridges–Markham" : 35059
+               , "Oakville" : 35060
+               , "Oshawa" : 35061
+               , "Ottawa Centre" : 35062
+               , "Ottawa–Orléans" : 35063
+               , "Ottawa South" : 35064
+               , "Ottawa–Vanier" : 35065
+               , "Ottawa West–Nepean" : 35066
+               , "Oxford" : 35067
+               , "Parkdale–High Park" : 35068
+               , "Parry Sound–Muskoka" : 35069
+               , "Perth–Wellington" : 35070
+               , "Peterborough" : 35071
+               , "Pickering–Scarborough East" : 35072
+               , "Prince Edward–Hastings" : 35073
+               , "Renfrew–Nipissing–Pembroke" : 35074
+               , "Richmond Hill" : 35075
+               , "St. Catharines" : 35076
+               , "St. Paul's" : 35077
+               , "Sarnia–Lambton" : 35078
+               , "Sault Ste. Marie" : 35079
+               , "Scarborough–Agincourt" : 35080
+               , "Scarborough Centre" : 35081
+               , "Scarborough–Guildwood" : 35082
+               , "Scarborough–Rouge River" : 35083
+               , "Scarborough Southwest" : 35084
+               , "Simcoe–Grey" : 35085
+               , "Simcoe North" : 35086
+               , "Stormont–Dundas–South Glengarry" : 35087
+               , "Sudbury" : 35088
+               , "Thornhill" : 35089
+               , "Thunder Bay–Rainy River" : 35090
+               , "Thunder Bay–Superior North" : 35091
+               , "Timmins–James Bay" : 35092
+               , "Toronto Centre" : 35093
+               , "Toronto–Danforth" : 35094
+               , "Trinity–Spadina" : 35095
+               , "Vaughan" : 35096
+               , "Welland" : 35097
+               , "Wellington–Halton Hills" : 35098
+               , "Whitby–Oshawa" : 35099
+               , "Willowdale" : 35100
+               , "Windsor–Tecumseh" : 35101
+               , "Windsor West" : 35102
+               , "York Centre" : 35103
+               , "York–Simcoe" : 35104
+               , "York South–Weston" : 35105
+               , "York West" : 35106
+               , "Brandon–Souris" : 46001
+               , "Charleswood–St. James–Assiniboia" : 46002
+               , "Churchill" : 46003
+               , "Dauphin–Swan River–Marquette" : 46004
+               , "Elmwood–Transcona" : 46005
+               , "Kildonan–St. Paul" : 46006
+               , "Portage–Lisgar" : 46007
+               , "Provencher" : 46008
+               , "Saint Boniface" : 46009
+               , "Selkirk–Interlake" : 46010
+               , "Winnipeg Centre" : 46011
+               , "Winnipeg North" : 46012
+               , "Winnipeg South" : 46013
+               , "Winnipeg South Centre" : 46014
+               , "Battlefords–Lloydminster" : 47001
+               , "Blackstrap" : 47002
+               , "Desnethé–Missinippi–Churchill River" : 47003
+               , "Cypress Hills–Grasslands" : 47004
+               , "Palliser" : 47005
+               , "Prince Albert" : 47006
+               , "Regina–Lumsden–Lake Centre" : 47007
+               , "Regina–Qu'Appelle" : 47008
+               , "Saskatoon–Humboldt" : 47009
+               , "Saskatoon–Rosetown–Biggar" : 47010
+               , "Saskatoon–Wanuskewin" : 47011
+               , "Souris–Moose Mountain" : 47012
+               , "Wascana" : 47013
+               , "Yorkton–Melville" : 47014
+               , "Fort McMurray–Athabasca" : 48001
+               , "Calgary East" : 48002
+               , "Calgary Centre-North" : 48003
+               , "Calgary Northeast" : 48004
+               , "Calgary–Nose Hill" : 48005
+               , "Calgary Centre" : 48006
+               , "Calgary Southeast" : 48007
+               , "Calgary Southwest" : 48008
+               , "Calgary West" : 48009
+               , "Crowfoot" : 48010
+               , "Edmonton–Mill Woods–Beaumont" : 48011
+               , "Edmonton Centre" : 48012
+               , "Edmonton East" : 48013
+               , "Edmonton–Leduc" : 48014
+               , "Edmonton–St. Albert" : 48015
+               , "Edmonton–Sherwood Park" : 48016
+               , "Edmonton–Spruce Grove" : 48017
+               , "Edmonton–Strathcona" : 48018
+               , "Lethbridge" : 48019
+               , "Macleod" : 48020
+               , "Medicine Hat" : 48021
+               , "Peace River" : 48022
+               , "Red Deer" : 48023
+               , "Vegreville–Wainwright" : 48024
+               , "Westlock–St. Paul" : 48025
+               , "Wetaskiwin" : 48026
+               , "Wild Rose" : 48027
+               , "Yellowhead" : 48028
+               , "Abbotsford" : 59001
+               , "Burnaby–Douglas" : 59002
+               , "Burnaby–New Westminster" : 59003
+               , "Cariboo–Prince George" : 59004
+               , "Chilliwack–Fraser Canyon" : 59005
+               , "Delta–Richmond East" : 59006
+               , "Pitt Meadows–Maple Ridge–Mission" : 59007
+               , "Esquimalt–Juan de Fuca" : 59008
+               , "Fleetwood–Port Kells" : 59009
+               , "Kamloops–Thompson–Cariboo" : 59010
+               , "Kelowna–Lake Country" : 59011
+               , "Kootenay–Columbia" : 59012
+               , "Langley" : 59013
+               , "Nanaimo–Alberni" : 59014
+               , "Nanaimo–Cowichan" : 59015
+               , "Newton–North Delta" : 59016
+               , "New Westminster–Coquitlam" : 59017
+               , "Okanagan–Shuswap" : 59018
+               , "North Vancouver" : 59019
+               , "Okanagan–Coquihalla" : 59020
+               , "Port Moody–Westwood–Port Coquitlam" : 59021
+               , "Prince George–Peace River" : 59022
+               , "Richmond" : 59023
+               , "Saanich–Gulf Islands" : 59024
+               , "Skeena–Bulkley Valley" : 59025
+               , "British Columbia Southern Interior" : 59026
+               , "South Surrey–White Rock–Cloverdale" : 59027
+               , "Surrey North" : 59028
+               , "Vancouver Centre" : 59029
+               , "Vancouver East" : 59030
+               , "Vancouver Island North" : 59031
+               , "Vancouver Kingsway" : 59032
+               , "Vancouver Quadra" : 59033
+               , "Vancouver South" : 59034
+               , "Victoria" : 59035
+               , "West Vancouver–Sunshine Coast–Sea to Sky Country" : 59036
+               , "Yukon" : 60001
+               , "Western Arctic" : 61001
+               , "Nunavut" : 62001
+               };
+
+var showvotes = function(key) {
+    if( $('li.' + key).hasClass('active') ) {
+        return;
+    }
+    $('li.active').toggleClass('active');
+    $('li.' + key).toggleClass('active');
+}
+
+var init = function() {
+    console.log("init");
+    var ridingnames = []
+    for( var name in fed_nums ) {
+        ridingnames.push(name);
+    }
+    $('#ridingselector').typeahead({
+      name: 'ridingselector'
+    //, prefetch: 'js/ridingnames.json'
+    , local: ridingnames
+    , limit: 10
+    });
+    $('a.libvotes').click(function() { showvotes('libvotes'); });
+    $('a.convotes').click(function() { showvotes('convotes'); });
+    $('a.ndpvotes').click(function() { showvotes('ndpvotes'); });
+    $('a.grnvotes').click(function() { showvotes('grnvotes'); });
+    $('a.blqvotes').click(function() { showvotes('blqvotes'); });
+    $('a.nonvotes').click(function() { showvotes('nonvotes'); });
+    showvotes('libvotes');
+};
+
+window.onload = init;
+
